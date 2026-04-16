fa5f2953a3fccfaf9a8b4f76d32254e0473f009e
Franciele Engel da Silva <francieleengel.fe@gmail.com>
2026-04-16 15:21:43 -0300

    Tipos

---
+++ tipos.js
@@ -0,0 +1,11 @@
+let nome="Franciele";
+let idade=30;
+let statusMatricula=true;
+let nota;
+let curso=null;
+
+console.log ("Nome do aluno: ", nome);
+console.log ("Idade: ", idade);
+console.log ("Está matriculada? ", statusMatricula);
+console.log ("Nota atual: ", nota);
+console.log ("Cusro: ", );
